3f899d0163f13ea60165697e617118bb6d3ee21e
Alex Martins <alex-3m@hotmail.com>
2025-03-13 00:22:05 -0300

    v9

--- app-DigNatural.py
+++ app-DigNatural.py
@@ -3,12 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import random
 
 def iniciar_driver():
     chrome_options = Options()
     # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
 
-    arguments = ['--lang=pt-BR', '--window-size=1000,1000', #800,600
+    arguments = ['--lang=pt-BR', '--window-size=800,600',
                 '--incognito']
     ''' Common arguments
     --start-maximized # Inicia maximizado
@@ -41,29 +42,22 @@ def iniciar_driver():
 
 driver = iniciar_driver()
 driver.get('https://cursoautomacao.netlify.app/desafios')
-#driver.maximize_window()
+driver.maximize_window()
 sleep(2)
 
-# Rolar X quantidade em pixels(descer)
-driver.execute_script("window.scrollTo(0, 500);")
-sleep(1)
+driver.execute_script("window.scrollTo(0, 1000)")
+sleep(2)
 
-opc_conversivel = driver.find_element(By.ID, 'conversivelcheckbox')
-opc_moto = driver.find_element(By.ID, 'motocheckbox')
-opc_offroad = driver.find_element(By.ID, 'offroadcheckbox')
+#texto = driver.find_element(By.XPATH, '//textarea[@id="campoparagrafo"]')
+texto = driver.find_element(By.ID, 'campoparagrafo')
+"""
+def digitar_naturalmente(texto, elemento):
+    for letra in texto:
+        elemento.send_keys(letra)
+        sleep(random.randint(1, 5)/30)
 
-if opc_conversivel.is_selected() == False:
-    opc_conversivel.click()
-    sleep(1)
-
-if opc_moto.is_selected() == True:
-    opc_moto.click()
-    sleep(1)
-    
-if opc_offroad.is_selected() == False:
-    opc_offroad.click()
-    sleep(1)
-    
-
+texto = 'Aqui recomendo que você sempre tenha essa história pronta para contar de uma forma que saliente o seu interesse por a área. Isso porque caso você realmente esteja interessado na vaga, desta maneira você irá mostrar a quem estiver te analisando que você tem uma grande probabilidade de ser um profissional mais engajado com as tarefas da empresa. Capriche nessa história.Muitos aqui iram dizer coisas como: porque vocês são uma ótima empresa, porque todos falam bem de você. Mas alerto: seja autêntico na sua resposta  as empresas querem aqui saber se você pesquisou sobre a empresa e como o seu skillset (seu conhecimento) pode agregar para a empresa, então use essa oportunidade para falar como a EMPRESA vai ganhar contratando você. Não diga coisas como: sempre foi meu sonho trabalhar aqui! As empresas não ganham nada com seu sonho de querer trabalhar lá (triste, mas é a realidade), então mostre o que você tem de melhor para ajudar a empresa crescer. No meu caso a resposta que dei foi a seguinte: “Amo aprender e ensinar e estou constantemente me atualizando, além de gostar muito de ensinar e aprender com os outros integrantes da equipe, percebi que vocês em uma cultura internacional e isso me chamou muita atenção, seria ótimo poder trabalhar com vocês!'
+digitar_naturalmente(texto,campo_texto)
+"""
 input('')
-driver.close
+driver.close()
