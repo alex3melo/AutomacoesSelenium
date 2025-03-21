5f88a39df3c5289fafb62e70a34f03cbf938f3a5
Alex Martins <alex-3m@hotmail.com>
2025-03-21 02:50:13 -0300

    v15-xml

---
+++ hsc-faturar-xml.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import xml.etree.ElementTree as et
+from lxml import etree
+
+
+tree = et.parse('Lote_49399.xml')
+raiz = tree.getroot()
+""""
+print("--------------------")
+print(f'raiz: {len(raiz)}')
+print(f'raiz filha: {len(raiz[1][0][1])}')
+print(raiz[1][0][1].tag)
+print("--------------------")
+"""
+guiasTISS = raiz[1][0][1]
+
+#Remover prefixo no nome da tag
+for elem in raiz.iter():
+    elem.tag = elem.tag[elem.tag.index('}')+1:]
+
+def lerDados(campos, guia):
+    lista = []
+    for campo in campos:
+        if(len(campo) == 0):
+            guia[str(campo.tag)] = str(campo.text)
+            print(campo.tag, campo.text, len(campo))
+        if(len(campo) > 0):
+            lerDados(campo, guia)
+
+    return guia
+
+def lerGuias(campos):
+    lista = []
+    
+    for campo in campos:
+        guia = {}
+
+        if(len(campo) == 0):
+            #guia[str(campo.tag)] = str(campo.text)
+            print(campo.tag, campo.text, len(campo))
+        if(len(campo) > 0):
+            guia = lerDados(campo, guia)
+
+        list.append(guia)
+
+    return lista
+
+#lerDados(guiasTISS)
+guias = lerGuias(guiasTISS)
+
+print('--------GUIA-------')
+print(f'Qnt Guias: {len(guias)}')
+
+
+
+
+"""
+data = []
+df = pd.DataFrame(data)
+
+df.to_csv('xml.csv', index=False, sep=';')
+print(df)"""
