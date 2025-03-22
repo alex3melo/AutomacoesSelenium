f6f0df29e6f692383152a50a66d63136df314a5e
Alex Martins <alex-3m@hotmail.com>
2025-03-21 23:12:18 -0300

    v17-ok

--- hsc-faturar-xml.py
+++ hsc-faturar-xml.py
@@ -2,16 +2,9 @@ import pandas as pd
 import xml.etree.ElementTree as et
 from lxml import etree
 
-
 tree = et.parse('Lote_49399.xml')
 raiz = tree.getroot()
-""""
-print("--------------------")
-print(f'raiz: {len(raiz)}')
-print(f'raiz filha: {len(raiz[1][0][1])}')
-print(raiz[1][0][1].tag)
-print("--------------------")
-"""
+
 guiasTISS = raiz[1][0][1]
 listaGuias = []
 guia = {}
@@ -20,42 +13,27 @@ guia = {}
 for elem in raiz.iter():
     elem.tag = elem.tag[elem.tag.index('}')+1:]
 
+#ler campos de cada guia
 def lerDados(campos, guia):
     for campo in campos:
         if(len(campo) == 0):
             guia[str(campo.tag)] = str(campo.text)
-            #print(guia[str(campo.tag)])
-            print(campo.tag, campo.text)
-        if(len(campo) > 0):
-            lerDados(campo)
-
-
-def lerGuias(campos):
-    for campo in campos:
-        guia = {}
-        if(len(campo) == 0):
-            #guia[str(campo.tag)] = str(campo.text)
-            print(campo.tag, campo.text)
+            #print(campo.tag, campo.text)
         if(len(campo) > 0):
             lerDados(campo, guia)
 
-        #listaGuias.append(guia)
-        print(guia)
+#ler nivem das guias, criar guias vazias
+def lerGuias(campos):
+    for campo in campos:
+        guia = {}
 
+        lerDados(campo, guia)
 
-#lerDados(guiasTISS)
+        listaGuias.append(guia)
+
 lerGuias(guiasTISS)
-"""guias = lerGuias(guiasTISS)
 
-print('--------GUIA-------')
-print(f'Qnt Guias: {len(guias)}')
-"""
-
-
-
-"""
-data = []
-df = pd.DataFrame(data)
+df = pd.DataFrame(listaGuias)
 
 df.to_csv('xml.csv', index=False, sep=';')
-print(df)"""
+print(df)
